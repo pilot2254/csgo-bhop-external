043ef75474105a94eb2a7117b4721b9ae0730c14
pilot2254 <109202734+pilot2254@users.noreply.github.com>
2025-08-04 19:01:49 +0200

    Final Cheat

---
+++ cheat/memory.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <Windows.h>
+#include <iostream>
+
+class Memory {
+
+private:
+	DWORD id = 0;			// ID of process
+	HANDLE process = NULL;	// Handle to process
+
+public:
+	Memory(const char* processName);
+	~Memory();
+
+	DWORD GetProcessId();
+	HANDLE GetProcessHandle();
+
+	uintptr_t GetModuleAddress(const char* moduleName);
+
+	template <typename T>
+	T Read(uintptr_t address) {
+		T value;
+		ReadProcessMemory(this->process, (LPCVOID)address, &value, sizeof(T), NULL);
+		return value;
+	}
+
+	template <typename T>
+	bool Write(uintptr_t address, T value) {
+		return WriteProcessMemory(this->process, (LPVOID)address, &value, sizeof(T), NULL);
+	}
+};
